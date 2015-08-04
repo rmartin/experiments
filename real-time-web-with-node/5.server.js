93b542cddb5d1b10d732a839e2ba35f5f9b85bf1
Roy Martin <roy.martin@modop.com>
2015-08-03 22:58:37 -0700

    Simulating async responses.

---
+++ real-time-web-with-node/5.server.js
@@ -0,0 +1,42 @@
+'use strict';
+var ASQ = require('asynquence'),
+	ASQR = require('asynquence-contrib');
+
+var handleHTTP = function(req, res) {
+	if (req.method === 'GET') {
+		if (req.url === '/') {
+			res.writeHead(200, {
+				'Content-type': 'text/plain'
+			});
+
+			ASQ(function(done) {
+					setTimeout(function() {
+						done(Math.random());
+					});
+				})
+				.then(function(done, msg) {
+					setTimeout(function() {
+						done('Hello World: ' + msg);
+					});
+				})
+				.then(function(done, msg) {
+					setTimeout(function() {
+						res.end(msg);
+					}, 1000);
+				});
+
+		} else {
+			res.writeHead(403);
+			res.end('Get outta here!');
+		}
+	} else {
+		res.writeHead(403);
+		res.end('Get outta here!');
+	}
+
+};
+
+var http = require('http'),
+	host = 'localhost',
+	port = 8006,
+	http_serv = http.createServer(handleHTTP).listen(port, host);
