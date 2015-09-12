5f5ebdc7b0ae4495bcf6cb8ac1ee2acac41244cf
Roy Martin <roy.martin@modop.com>
2015-09-12 14:39:10 -0700

    Created a simple example project to capture a screenshot from a website and stream it out to the browser without writing to disk.

---
+++ code/phantomjs-static-image/assets/scripts/main.js
@@ -0,0 +1,3 @@
+$('.info-overlay-content').each(function(i,e){
+    $(this).html('Hello World ' + (i+1));
+})
---
+++ code/phantomjs-static-image/index.js
@@ -0,0 +1,32 @@
+'use strict';
+var webshot = require('webshot'),
+    express = require('express'),
+    app = express(),
+    options = {
+        screenSize: {
+            width: 1024,
+            height: 500
+        },
+        shotSize: {
+            width: 'all',
+            height: 'all'
+        }
+    };
+
+app.get('/', function(req, res) {
+    var renderStream = webshot('http://localhost:3333/', null, options),
+        screenshot = '';
+
+    // Capture the streaming output from the screenshot
+    renderStream.on('data', function(data) {
+        screenshot += data.toString('binary');
+    });
+
+    // Once the image capture is completed, write it out to the browser
+    renderStream.on('end', function() {
+        res.set('Content-Type', 'image/png');
+        res.end(screenshot, 'binary');
+    });
+});
+
+app.listen(3000);
