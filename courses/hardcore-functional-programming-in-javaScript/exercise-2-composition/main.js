ad7ad2c4961432a5fc899db24966b192f5d87287
Roy Martin <roy.martin@modop.com>
2015-09-26 00:08:39 -0700

    Added additional answers to the composition example.

--- courses/hardcore-functional-programming-in-javaScript/exercise-2-composition/main.js
+++ courses/hardcore-functional-programming-in-javaScript/exercise-2-composition/main.js
@@ -54,7 +54,9 @@ assertEqualArrays(
 // Use the names function you wrote above
 // with _.compose and _.contains.
 
-var isAuthor = _.identity; // change this
+var isAuthor = function(name, articles){
+    return _.compose(_.contains(name), names)(articles);
+};
 assertEqual(
     false,
     isAuthor('New Guy', articles)
@@ -83,7 +85,7 @@ var fork = _.curry(function(lastly, f, g, x) {
 // compute the average values in a list using
 // only fork, _.divide, _.sum, and _.size.
 
-var avg = _.identity; // change this
+var avg = fork(_.divide, _.sum, _.size); // change this
 assertEqual(3, avg([1, 2, 3, 4, 5]));
 
 
